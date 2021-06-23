1330bc2e27646394cbd2031ab2e47449abeb88c6
Raghav Mangal <raghavmangal22@gmail.com>
2021-06-23 15:17:10 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path');
+require('dotenv').config();
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server connected on PORT ${PORT}`));
+
+mongoose.connect(
+  process.env.MONGOOSE_CONNECTION_URI,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err) => {
+    if (err) {
+      return console.error(err);
+    }
+    console.log('MONGODB CONNECTION ESTABILSHED');
+  }
+);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+app.use('/superadmins', require('./routes/superadmins'));
